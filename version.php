f38969002f3e737f83c338a6fb42f3396e50c53c
spiritscout <chris@bowenehs.com>
2014-05-19 17:01:19 -0400

    version bump

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2012123001;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2012123003;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2012062100;  // Requires this Moodle version
 $module->cron     = 0;           // Period for cron to check this module (secs)
 ?>
